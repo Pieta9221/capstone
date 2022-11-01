fb4fefbd7402af9450943802c741692f66f35ef7
Pieta9221 <mercy_nwaodu@yahoo.com>
2022-11-01 12:41:54 +0100

    update

---
+++ main.js
@@ -0,0 +1,32 @@
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-home")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("top")
+  } else {
+    backToTopButton.classList.add("top")
+  }
+})
+
+ 
+
+
+
+
+var navLinks = document.getElementById("nav-links");
+
+      function showMenu() {
+        // navLinks.style.right = "0";
+        navLinks.style.display = "block";
+      }
+      function hideMenu() {
+        //navLinks.style.right = "-200px";
+        navLinks.style.display = "none";
+      }
+
+     
